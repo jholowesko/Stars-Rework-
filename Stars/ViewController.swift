27a36c93b42f571f7bd9b35d045bbcf32d8b74cf
John Holowesko <john@sipsip.io>
2019-12-11 16:40:51 -0500

    Initial Commit

---
+++ Stars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stars
+//
+//  Created by John Holowesko on 12/11/19.
+//  Copyright © 2019 John Holowesko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
